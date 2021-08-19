ead88ab8f411b994e48733c21918832d0039a189
elysee15 <elyseebleu1@gmail.com>
2021-08-19 18:34:45 +0000

    feat: add maintainance mode screen

--- src/pages/MaintainancePage.tsx
+++ src/pages/MaintainancePage.tsx
@@ -1,10 +1,31 @@
-import { Typography } from "@material-ui/core";
+import { Box, makeStyles, Theme, Typography } from "@material-ui/core";
+
+const useStyles = makeStyles((theme: Theme) => ({
+	container: {
+		position: "absolute",
+		top: "50%",
+		left: "50%",
+		transform: "translate(-50%, -50%)",
+		color: "#222121",
+		padding: theme.spacing(2)
+	},
+	fontBold: {
+		fontWeight: "bold"
+	},
+	textGray: {}
+}));
 
 const MaintainancePage: React.FC = () => {
+	const classes = useStyles();
+
 	return (
-		<div>
-			<Typography>We&apos;ll back soon !</Typography>
-			<Typography>Sorry for the inconvenience but we&apos;re performing some maintainance at the moment</Typography>
+		<div className={classes.container}>
+			<Box maxWidth="500px">
+				<Typography variant="h3" className={classes.fontBold}>
+					We&apos;ll be back soon !
+				</Typography>
+				<Typography>Sorry for the inconvenience but we&apos;re performing some maintainance at the moment</Typography>
+			</Box>
 		</div>
 	);
 };
